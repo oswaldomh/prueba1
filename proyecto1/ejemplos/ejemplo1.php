dea46f6614017171cae18bc64dfdefff1cf08e31
oswaldomh <oswaldo.maldonado.888@gmail.com>
2022-12-27 14:09:27 -0600

    ah perro traes el ovnitrix cambios subidos

---
+++ proyecto1/ejemplos/ejemplo1.php
@@ -0,0 +1,30 @@
+<?php
+// Este es un comentario
+
+/*También es un comentario */
+
+
+echo "uno";
+?>
+<br>
+<?php
+print "dos";
+?>
+<br>
+<?php
+echo "tres";
+?>
+<br>
+<?php
+echo "cuatro";
+?>
+<br>
+<?php
+echo "cinco";
+?>
+<br>
+<?php
+echo "seis";
+?>
+
+
